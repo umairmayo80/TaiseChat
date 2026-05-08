21483fee3b3f8494baed672a1d15ca74672fdead
umairmayo80 <umairmayo1080@gmail.com>
2026-05-08 12:47:05 +0500

    🚀 feat: Update footer branding and improve footer text for clarity

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -27,9 +27,9 @@ function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
+      : 'TaiseChat ' +
         Constants.VERSION +
-        '](https://librechat.ai) - ' +
+        ' - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
